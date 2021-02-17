4b8826fb23e373d76f0688de36a0200cd0649997
Brody Bayley <brodybayley@gmail.com>
2021-02-17 00:33:58 +0000

    added cookie parser and began adding a login label in header

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
