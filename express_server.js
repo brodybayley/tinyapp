801504b55388dc0a889970a9c0ee6911f7bf6ff1
Brody Bayley <brodybayley@gmail.com>
2021-02-19 05:55:27 +0000

    deleted commented code from file

--- express_server.js
+++ express_server.js
@@ -178,17 +178,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
